1e68aab7521a7a9412832084b9bbaf61f970d60a
jung min jun <alswns7984@naver.com>
2022-03-05 21:49:38 +0900

    텍스트 형식 메시지받기

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic.request;
+
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+/**
+ * 택스트 형식 메시지 받는 방법
+ */
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        ServletInputStream inputStream = request.getInputStream(); //byte 코드를 문자로 변환 문자표를 지정해줘야함
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("ok");
+    }
+
+}
